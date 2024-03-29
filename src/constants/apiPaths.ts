c4c7f98756e110fce245736d761c92948a9bde7e
Barys Marukhin <barys_marukhin@epam.com>
2024-03-29 20:05:41 +0400

    Feat: Task 3. Api for PLP.

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,4 +1,5 @@
 const API_PATHS = {
+  root: "https://y8mr3ffngi.execute-api.us-east-1.amazonaws.com",
   product: "https://.execute-api.eu-west-1.amazonaws.com/dev",
   order: "https://.execute-api.eu-west-1.amazonaws.com/dev",
   import: "https://.execute-api.eu-west-1.amazonaws.com/dev",
